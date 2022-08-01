e4afd2c40a81d715230fc43f0e8c6045ba34275a
Chris Contolini <contolini@users.noreply.github.com>
2022-08-01 17:32:28 -0400

    Add IntersectionObserver test case

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,21 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+  const blue = 'rgb(0, 0, 255)'
+
+  it('passes when IntersectionObserver\'s `root` is `document`', () => {
+    cy.viewport(500, 100)
+
+    cy.visit('index.html')
+    cy.get('#last-paragraph').scrollIntoView({ duration: 3000 });
+    cy.get('#last-paragraph').should('have.css', 'color', blue);
   })
+
+  it('fails when IntersectionObserver\'s `root` is `null`', () => {
+    cy.viewport(500, 100)
+
+    cy.visit('index.html?noroot')
+    cy.get('#last-paragraph').scrollIntoView({ duration: 3000 });
+    cy.get('#last-paragraph').should('have.css', 'color', blue);
+  })
+
 })
